5ffcc8661dc3e29fcc8b263740b204c90c5289bb
georgedoherty123 <gmdoherty@cps.edu>
2017-03-14 23:50:37 +0000

    project2

--- 8/sw.js
+++ 8/sw.js
@@ -16,6 +16,9 @@ return cache.addAll([
 });
 self.addEventListener('fetch', function(event) {
 console.log(event.request.url);
+});
+self.addEventListener('fetch', function(event) {
+console.log(event.request.url);
 event.respondWith(
 caches.match(event.request).then(function(response) {
 return response || fetch(event.request);
